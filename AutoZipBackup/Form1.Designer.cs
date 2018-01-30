5acf87e861578a24591cbd692b1bc0a231a0010e
Zufixx <filip.0717@hotmail.com>
2018-01-30 15:48:41 +0100

    Initial Commit. Main functionality works.

---
+++ AutoZipBackup/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace AutoZipBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.fromButton = new System.Windows.Forms.Button();
+            this.toButton = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.fromBox = new System.Windows.Forms.TextBox();
+            this.toBox = new System.Windows.Forms.TextBox();
+            this.zippedCheck = new System.Windows.Forms.CheckBox();
+            this.branchBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // fromButton
+            // 
+            this.fromButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fromButton.Location = new System.Drawing.Point(347, 33);
+            this.fromButton.Name = "fromButton";
+            this.fromButton.Size = new System.Drawing.Size(75, 23);
+            this.fromButton.TabIndex = 0;
+            this.fromButton.Text = "Browse";
+            this.fromButton.UseVisualStyleBackColor = true;
+            this.fromButton.Click += new System.EventHandler(this.fromButton_Click);
+            // 
+            // toButton
+            // 
+            this.toButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.toButton.Location = new System.Drawing.Point(347, 62);
+            this.toButton.Name = "toButton";
+            this.toButton.Size = new System.Drawing.Size(75, 23);
+            this.toButton.TabIndex = 1;
+            this.toButton.Text = "Browse";
+            this.toButton.UseVisualStyleBackColor = true;
+            this.toButton.Click += new System.EventHandler(this.toButton_Click);
+            // 
+            // copyButton
+            // 
+            this.copyButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.copyButton.Location = new System.Drawing.Point(12, 119);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(410, 23);
+            this.copyButton.TabIndex = 2;
+            this.copyButton.Text = "Make a backup";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // fromBox
+            // 
+            this.fromBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fromBox.Location = new System.Drawing.Point(62, 35);
+            this.fromBox.Name = "fromBox";
+            this.fromBox.Size = new System.Drawing.Size(279, 20);
+            this.fromBox.TabIndex = 3;
+            // 
+            // toBox
+            // 
+            this.toBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.toBox.Location = new System.Drawing.Point(62, 64);
+            this.toBox.Name = "toBox";
+            this.toBox.Size = new System.Drawing.Size(279, 20);
+            this.toBox.TabIndex = 4;
+            // 
+            // zippedCheck
+            // 
+            this.zippedCheck.AutoSize = true;
+            this.zippedCheck.Checked = true;
+            this.zippedCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.zippedCheck.Location = new System.Drawing.Point(12, 12);
+            this.zippedCheck.Name = "zippedCheck";
+            this.zippedCheck.Size = new System.Drawing.Size(65, 17);
+            this.zippedCheck.TabIndex = 6;
+            this.zippedCheck.Text = "Zipped?";
+            this.zippedCheck.UseVisualStyleBackColor = true;
+            this.zippedCheck.CheckedChanged += new System.EventHandler(this.zippedCheck_CheckedChanged);
+            // 
+            // branchBox
+            // 
+            this.branchBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.branchBox.Location = new System.Drawing.Point(62, 93);
+            this.branchBox.Name = "branchBox";
+            this.branchBox.Size = new System.Drawing.Size(360, 20);
+            this.branchBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "To:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Branch:";
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.Location = new System.Drawing.Point(12, 148);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.output.Size = new System.Drawing.Size(410, 96);
+            this.output.TabIndex = 11;
+            this.output.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 255);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.branchBox);
+            this.Controls.Add(this.zippedCheck);
+            this.Controls.Add(this.toBox);
+            this.Controls.Add(this.fromBox);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.toButton);
+            this.Controls.Add(this.fromButton);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "AutoZipBackup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button fromButton;
+        private System.Windows.Forms.Button toButton;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.TextBox fromBox;
+        private System.Windows.Forms.TextBox toBox;
+        private System.Windows.Forms.CheckBox zippedCheck;
+        private System.Windows.Forms.TextBox branchBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox output;
+    }
+}
+
---
+++ AutoZipBackup/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace AutoZipBackup
+{
+    public partial class Form1 : Form
+    {
+        private string fromDirectory = @"";
+        private string toDirectory = @"";
+
+        private bool zipped = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            copyButton.Enabled = false;
+            fromBox.Text = fromDirectory;
+            toBox.Text = toDirectory;
+            output.Text = "Select folders to copy to and from. (Optional) Add the branch name\n";
+            branchBox.Text = "master";
+
+            if (fromDirectory != null && toDirectory != null)
+                copyButton.Enabled = true;
+            else
+                copyButton.Enabled = false;
+        }
+
+        private void fromButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                fromDirectory = folderBrowserDialog.SelectedPath;
+                fromBox.Text = fromDirectory;
+            }
+            else
+                return;
+
+            if (fromDirectory != null && toDirectory != null)
+                copyButton.Enabled = true;
+            else
+                copyButton.Enabled = false;
+
+        }
+
+        private void toButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                toDirectory = folderBrowserDialog.SelectedPath;
+                toBox.Text = toDirectory;
+            }
+            else
+                return;
+
+            if (fromDirectory != null && toDirectory != null)
+                copyButton.Enabled = true;
+            else
+                copyButton.Enabled = false;
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            if (fromDirectory == null && toDirectory == null)
+                return;
+
+            copyButton.Enabled = false;
+
+            string zipPath = @"C:\temp\tempBackup.zip";
+            toDirectory = toDirectory + "/" + DateTime.Today.ToString("d");
+            if (zipped)
+            {
+                if(branchBox.Text != "")
+                    zipPath = toDirectory + " " + branchBox.Text + ".zip";
+                else
+                    zipPath = toDirectory + ".zip";
+
+                output.Text += DateTime.Now.ToString("HH:mm:ss") + ": Zipping Files. Please wait.\n";
+                if (File.Exists(zipPath))
+                    File.Delete(zipPath);
+                ZipFile.CreateFromDirectory(fromDirectory, zipPath);
+                output.Text += DateTime.Now.ToString("HH:mm:ss") + ": Done!\n\n";
+            }
+            else
+            {
+                if (branchBox.Text != "")
+                    toDirectory += " " + branchBox.Text;
+
+                if (File.Exists(zipPath))
+                    File.Delete(zipPath);
+
+                output.Text += DateTime.Now.ToString("HH:mm:ss") + ": Zipping Files. Please wait.\n";
+                ZipFile.CreateFromDirectory(fromDirectory, zipPath);
+                output.Text += DateTime.Now.ToString("HH:mm:ss") + ": Extracting files. Please wait.\n";
+
+                if (Directory.Exists(toDirectory))
+                    Directory.Delete(toDirectory);
+
+                ZipFile.ExtractToDirectory(zipPath, toDirectory);
+                output.Text += DateTime.Now.ToString("HH:mm:ss") + ": Deleting temporary zip folder.\n";
+                File.Delete(zipPath);
+                output.Text += DateTime.Now.ToString("HH:mm:ss") + ": Done!\n\n";
+            }
+
+            copyButton.Enabled = true;
+        }
+
+        private void zippedCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            zipped = zippedCheck.Checked;
+        }
+    }
+}
